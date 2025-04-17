2f57b62013bfb677b89a821946a05e5bc7fb1100
binoympappachen <43516209+binoympappachen@users.noreply.github.com>
2025-04-18 00:54:58 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,10 +3,21 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 import os
 
+#Load Dataset
+
 X, y = load_iris(return_X_y=True)
+
+#Train model
 model = LogisticRegression(max_iter=200)
 model.fit(X, y)
 
+# Create folder if does not exists
+
 os.makedirs("model", exist_ok=True)
+
+# Save the model
+
 with open("model/model.pkl", "wb") as f:
     pickle.dump(model, f)
+    
+print("model.pkl created successfully!")
